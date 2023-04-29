6bacece59c1b340efffd4d3446393bb7338d6d19
sejalpatel-gunict <sp03@ganpatuniversity.ac.in>
2023-04-29 21:28:19 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { FormsModule } from '@angular/forms';
+import { ColorDirective } from './directive/color.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    ColorDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
